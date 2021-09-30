17c0c9d0b65cff161a9ff5102942f8fbf0f53c8b
PreethamKarnekanti <preetham.karnekanti@zemosolabs.com>
2021-09-30 16:34:15 +0530

    Added favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+import Rails, { $ } from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import 'stylesheets/semantic-ui.scss'
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', function(){
+    $('.ui.dropdown').dropdown();
+})
