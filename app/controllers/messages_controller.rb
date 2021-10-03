a212fcb1de47931761fc6446abb9ebe62860bffe
PreethamKarnekanti <preetham.karnekanti@zemosolabs.com>
2021-10-03 11:17:09 +0530

    channel

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+    before_action :require_user
+
+    def create
+        message = Message.new(message_params)
+        message.user = current_user
+        if message.save
+            ActionCable.server.broadcast "chatroom_channel",
+                                            mod_message: message_render(message)
+        else
+            flash[:error] = "You must login to send the message"
+            render "index"
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:body)
+    end
+
+    def message_render(message)
+        render(partial: "message",locals: {message: message})
+    end
+end
